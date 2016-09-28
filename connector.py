e59386d981ddc6927433f593ddf349f48ce681f7
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-09-28 12:13:36 +0300

    FIX behavior of connector

--- connector.py
+++ connector.py
@@ -1,46 +1,50 @@
-import pty
 import select
 import shlex
 import subprocess
 import sys
-import os
+
+import pyte
+
+STOP_SIGNAL = '@'
 
 
-def connect(exe=None, in_stream=sys.stdin, out_stream=sys.stdout):
+def connect(exe=None, size=(80, 24)):
     if exe is None:
         exe = ['python3']
     if isinstance(exe, str):
         exe = shlex.split(exe)
-    master, slave = pty.openpty()
-    p = subprocess.Popen(exe, stdin=slave, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    pin = os.fdopen(master, 'w')
+
+    p = subprocess.Popen(exe, stdout=subprocess.PIPE)
+
+    screen = pyte.Screen(*size)
+    screen.set_mode(pyte.screens.mo.LNM)
+    stream = pyte.Stream()
+    stream.attach(screen)
+
+    def print_display():
+        sys.stdout.write('Screen starts\n')
+        for lineno, line in enumerate(screen.display):
+            sys.stdout.write('{:2d}: {}\n'.format(lineno, line))
+        sys.stdout.write('Screen ends\n\n')
 
     msg = ''
-    errmsg = ''
-
     while True:
-        rs, ws, es = select.select([in_stream, p.stdout, p.stderr], [], [])
-        if in_stream in rs:
-            c = in_stream.read(1)
+        rs, ws, es = select.select([p.stdout], [], [])
+        if p.stdout in rs:
+            c = p.stdout.read(1).decode()
+            if c == STOP_SIGNAL:
+                break
             if c == '':
                 msg = msg[:-1]
             elif c == '\n':
-                pin.write(msg + '\n')
-                # out_stream.write('{}\n'.format(msg))
+                stream.feed(msg + '\n')
                 msg = ''
+                print_display()
             else:
                 msg += c
-                # out_stream.flush()
-        if p.stdout in rs:
-            out_stream.write(p.stdout.readline().decode())
-            # sys.stderr.flush()
-        if p.stderr in rs:
-            errmsg += p.stderr.read(1).decode()
-            if errmsg.endswith('>>> '):
-                errmsg = errmsg[:-4]
-            if errmsg.endswith('\n'):
-                out_stream.write('{}\n'.format(errmsg))
-                errmsg = ''
 
 if __name__ == '__main__':
-    connect('python3 pyte_wrapper.py 20 5')
+    if len(sys.argv) < 2:
+        print('there must be executable path')
+        exit(0)
+    connect(sys.argv[1:], (20, 20))
