465f5d38b2e1d701e05fc26d68a27cbea5eea972
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-10-12 00:39:54 +0000

    ENH: add connection to bash

--- network/server.py
+++ network/server.py
@@ -1,39 +1,58 @@
 import json
 import os
 import pty
+import subprocess
 import select
 import shlex
-import subprocess
 import sys
 from collections import namedtuple
 
 import aiohttp
+import asyncio
 import pyte
 from aiohttp import web
-from . import async_connector
+
 
 Size = namedtuple('Size', ['width', 'height'])
 DEFAULT_SIZE = Size(80, 24)
 size = DEFAULT_SIZE
-
+exe = ['bash']
 
 async def websocket_handler(request):
-
     ws = web.WebSocketResponse()
     await ws.prepare(request)
+
+    screen = pyte.Screen(*DEFAULT_SIZE)
+    screen.set_mode(pyte.screens.mo.LNM)
+    stream = pyte.Stream()
+    stream.attach(screen)
+
+    master_fd, slave_fd = pty.openpty()
+    p = subprocess.Popen(exe, stdin=slave_fd, stdout=slave_fd,
+                         stderr=subprocess.STDOUT, close_fds=True)
+    os.close(slave_fd)
+    p_out = os.fdopen(master_fd, 'w+b', 0)
+
+    def process_out_handler():
+        b_data = p_out.read(32)
+        data = b_data.decode()
+        stream.feed(data)
+        answer = prepare_screen(screen)
+        ws.send_str(answer)
+
+    loop = asyncio.get_event_loop()
+    loop.add_reader(p_out, process_out_handler)
+    # loop.run_forever()
 
     async for msg in ws:
         if msg.type == aiohttp.WSMsgType.TEXT:
             if msg.data == 'close':
                 await ws.close()
             else:
-                answer = display(msg.data)
-                # answer = async_connector.run(msg.data)
-                ws.send_str(answer)
+                p_out.write(msg.data.encode())
         elif msg.type == aiohttp.WSMsgType.ERROR:
             print('ws connection closed with exception %s' %
                   ws.exception())
-
     print('websocket connection closed')
 
     return ws
@@ -67,59 +86,11 @@ def prepare_screen(screen):
     return json.dumps({'screen': screen.display, 'cursor': {'x': screen.cursor.x, 'y': screen.cursor.y}})
 
 
-def display(data):
-    exe = shlex.split(data)
-    screen = pyte.Screen(*size)
-    screen.set_mode(pyte.screens.mo.LNM)
-    stream = pyte.Stream()
-    stream.attach(screen)
-
-    result_display = None
-
-    master_fd, slave_fd = pty.openpty()
-    p = subprocess.Popen(exe, stdin=slave_fd, stdout=slave_fd,
-                         stderr=subprocess.STDOUT, close_fds=True)
-    os.close(slave_fd)
-    p_out = os.fdopen(master_fd, 'w+b', 0)
-
-    works = True
-
-    while works:
-        rs, ws, es = select.select([sys.stdin, p_out], [], [])
-        for r in rs:
-            if r is p_out:
-                try:
-                    line = p_out.read(1000)
-                    stream.feed(line.decode())
-                    result_display = screen.display
-                except OSError as e:
-                    if e.errno == 5:
-                        works = False
-                    else:
-                        raise e
-            if r is sys.stdin:
-                line = sys.stdin.readline()
-                p_out.write(line.encode())
-
-        if p.poll():
-            break
-
-    def format_display():
-        if result_display is None:
-            return None
-        res = ''
-        for i, line in enumerate(result_display):
-            res += '{}: {}\n'.format(i, line)
-        return res
-
-    return format_display()
-
-
 def start_server():
     server_folder = os.path.dirname(__file__)
     app = web.Application()
-    # app.router.add_get('/ws', websocket_handler)
-    app.router.add_get('/ws', ws_command_line_handler)
+    app.router.add_get('/ws', websocket_handler)
+    # app.router.add_get('/ws', ws_command_line_handler)
     app.router.add_static('/', server_folder + '/static', show_index=True)
 
     web.run_app(app)
