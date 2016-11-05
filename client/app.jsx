094c670c04a161482cd8dac9ed665595b9c08a4e
Dmitriy Novozhilov <dimon2x@gmail.com>
2016-11-05 15:45:53 +0000

    BUG: client script fixed

--- client/app.jsx
+++ client/app.jsx
@@ -6,11 +6,8 @@ const DEFAULT_HEIGHT = 24;
 const serverUrl = 'ws://0.0.0.0:8080/ws';
 
 $(function () {
-    // const terminal = Terminal();
-    // terminal.render();
     React.render(<Terminal/>, document.body);
-    // $("#terminal").focus()
-
+    $("#terminal").focus()
 });
 
 var Cell = React.createClass({
@@ -24,13 +21,13 @@ var Cell = React.createClass({
 
     setFgColor: function(color) {
         this.setState({
-            fg: color
+            fg: 'fg-' + color
         });
     },
 
     setBgColor: function(color) {
         this.setState({
-            bg: color
+            bg: 'bg-' + color
         });
     },
 
@@ -67,13 +64,6 @@ var Terminal = React.createClass({
     getInitialState() {
         let width = DEFAULT_WIDTH;
         let height = DEFAULT_HEIGHT;
-        this.screen = new Array(height);
-        for (let i = 0; i < DEFAULT_HEIGHT; i++) {
-            this.screen[i] = new Array(width);
-            for (let j = 0; j < width; j++) {
-                this.screen[i][j] = null;
-            }
-        }
         return {
             cursor: {'x': 0, 'y': 0},
             width: width,
@@ -93,19 +83,16 @@ var Terminal = React.createClass({
     },
 
     send(message, type) {
-        this.socket.send(message);
+        this.state.socket.send(message);
     },
 
-    eraseCursor() {
-        let {x, y} = this.state.cursor;
-        if (y < this.state.height && x < this.state.width) {
-            this.state.screen[y][x].setDefaultColor();
-        }
+    getCell(i, j) {
+        return this.refs['cell_' + i + '_' + j]
     },
 
     receiveMessage(message) {
         let {cursor: {x: cx, y: cy}, screen, dirty} = message;
-        if (cx == this.width) {
+        if (cx == this.state.width) {
             cx = 0;
             cy++;
         }
@@ -113,12 +100,13 @@ var Terminal = React.createClass({
         this.eraseCursor();
         for (let i of dirty) {
             for (let j = 0; j < this.state.width; j++) {
-                const cell = this.screen[i][j];
+                // const cell = this.screen[i][j];
+                const cell = this.getCell(i, j);
                 let [data, fg, bg,
                     bold, italics, underscore, strikethrough,
                     reverse] = screen[i][j];
 
-                // console.log(data);
+                // console.log('cell ' + cell.props.x + ' ' + cell.props.y);
                 cell.setSymbol(data);
 
                 cell.setFgColor(fg);
@@ -128,7 +116,7 @@ var Terminal = React.createClass({
                     cell.setReverse();
             }
         }
-        this.updateCursor();
+        this.updateCursor(cx, cy);
     },
 
     updateCursor(x, y) {
@@ -136,7 +124,16 @@ var Terminal = React.createClass({
             cursor: {x: x, y: y}
         });
         if (y < this.state.height && x < this.state.width) {
-            this.state.screen[y][x].setReverseColor();
+            // this.screen[y][x].setReverseColor();
+            this.getCell(y, x).setReverseColor();
+        }
+    },
+
+    eraseCursor() {
+        let {x, y} = this.state.cursor;
+        if (y < this.state.height && x < this.state.width) {
+            // this.screen[y][x].setDefaultColor();
+            this.getCell(y, x).setDefaultColor();
         }
     },
 
@@ -149,6 +146,13 @@ var Terminal = React.createClass({
         }
     },
 
+    onKeyPress(e) {
+        var message = keyToMessage(e);
+        if (message) {
+            this.send(message, 'command');
+        }
+    },
+
     render: function () {
         let rows = [];
         for (let i = 0; i < this.state.height; i++) {
@@ -158,16 +162,18 @@ var Terminal = React.createClass({
                     key={'(' + i + ',' + j + ')'}
                     x={i}
                     y={j}
-                    ref={(cell) => {
-                        console.log(cell.props.x);
-                        this.screen[cell.props.x][cell.props.y] = cell;
-                    }}
+                    ref={'cell_' + i + '_' + j}
+
                 />);
             }
             rows.push(<tr key={'row ' + i}>{row}</tr>);
         }
         return (
-            <div id="terminal" tabIndex="1">
+            <div id="terminal"
+                 tabIndex="1"
+                 onKeyDown={this.onKeyDown}
+                 onKeyPress={this.onKeyPress}
+            >
                 <table id="screen" frame="border">
                     {rows}
                 </table>
@@ -245,3 +251,68 @@ function keyToMessage(e) {
 
     return message;
 }
+
+const ctrl = {
+    LF: '\n',
+    BACKSPACE: '\u0008',
+    ESC: '\u001b',
+    CSI: '\u001b[',
+    TAB: '\u0009',
+    HOME: '\u001b[1~',
+    INSERT: '\u001b[2~',
+    // DEL: '\u007f', it's for VT100
+    DEL: '\u001b[3~',
+    END: '\u001b[4~',
+    PAGE_UP: '\u001b[5~',
+    PAGE_DOWN: '\u001b[6~',
+    UP_ARROW: '\u001b[A',
+    DOWN_ARROW: '\u001b[B',
+    RIGHT_ARROW: '\u001b[C',
+    LEFT_ARROW: '\u001b[D',
+};
+
+// *F keys* for linux terminal
+const fKeys = {
+    1: ctrl.CSI + '[A',
+    2: ctrl.CSI + '[B',
+    3: ctrl.CSI + '[C',
+    4: ctrl.CSI + '[D',
+    5: ctrl.CSI + '[E',
+    6: ctrl.CSI + '17~',
+    7: ctrl.CSI + '18~',
+    8: ctrl.CSI + '19~',
+    9: ctrl.CSI + '20~',
+    10: ctrl.CSI + '21~',
+    11: ctrl.CSI + '23~',
+    12: ctrl.CSI + '24~'
+};
+
+
+// *F keys* for vt220 terminal
+// const fKeys = {
+//     1: ctrl.ESC + 'OP',
+//     2: ctrl.ESC + 'OQ',
+//     3: ctrl.ESC + 'OR',
+//     4: ctrl.ESC + 'OS',
+//     5: ctrl.CSI + '15~',
+//     6: ctrl.CSI + '17~',
+//     7: ctrl.CSI + '18~',
+//     8: ctrl.CSI + '19~',
+//     9: ctrl.CSI + '20~',
+//     10: ctrl.CSI + '21~',
+//     11: ctrl.CSI + '23~',
+//     12: ctrl.CSI + '24~'
+// };
+
+const controlNumberKeys = {
+    0: '\u0030',
+    1: '\u0031',
+    2: '\u0000',
+    3: '\u001b',
+    4: '\u001c',
+    5: '\u001d',
+    6: '\u001e',
+    7: '\u001f',
+    8: '\u007f',
+    9: '\u0039',
+};
